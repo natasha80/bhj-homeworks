20fdc604e1be1f785d20435696133cef13b81b63
natasha80 <61574584+natasha80@users.noreply.github.com>
2021-04-25 17:11:49 +0700

    first commit menu

---
+++ element-search/menu/task.js
@@ -0,0 +1,27 @@
+"use strict";
+
+const menuLink = document.getElementsByClassName("menu__link");
+const menuSub = document.querySelectorAll("ul.menu_sub");
+
+function removeActive() {
+  for (let i = 0; i < menuSub.length; i++) {
+    menuSub[i].classList.remove("menu_active");
+  }
+}
+
+for (let i = 0; i < menuLink.length; i++) {
+  menuLink[i].onclick = function() {
+    const element = menuLink[i].nextElementSibling.classList;
+
+    if (element.contains("menu_active")) {
+      removeActive();
+      return false;
+    }
+
+    if (element.contains("menu_sub")) {
+      removeActive();
+      element.add("menu_active");
+      return false;
+    }
+  };
+}
