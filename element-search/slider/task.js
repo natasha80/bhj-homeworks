3cf6b579d5f627682361a390dfe8845fe52ddaa4
natasha80 <61574584+natasha80@users.noreply.github.com>
2021-04-25 17:22:59 +0700

    first commit slider

---
+++ element-search/slider/task.js
@@ -0,0 +1,38 @@
+"use strict";
+
+const prevButton = document.querySelector(".slider__arrow_prev");
+const nextButton = document.querySelector(".slider__arrow_next");
+
+let slideNumber = 0;
+
+const sliderImages = document.getElementsByClassName("slider__item");
+let sliderImage = sliderImages.item(slideNumber);
+
+function showSlides(n) {
+  sliderImage = sliderImages.item(slideNumber + n);
+  sliderImage.classList.remove("slider__item_active");
+  sliderImage = sliderImages.item(slideNumber);
+  sliderImage.classList.add("slider__item_active");
+}
+
+nextButton.onclick = function() {
+  if (slideNumber >= 0 && slideNumber < sliderImages.length - 1) {
+    slideNumber++;
+    showSlides(-1);
+  } else {
+    slideNumber = 0;
+    sliderImage.classList.remove("slider__item_active");
+    showSlides();
+  }
+};
+
+prevButton.onclick = function() {
+  if (slideNumber < sliderImages.length && slideNumber > 0) {
+    slideNumber--;
+    showSlides(1);
+  } else {
+    slideNumber = sliderImages.length - 1;
+    sliderImage = sliderImages.item(0);
+    showSlides();
+  }
+};
